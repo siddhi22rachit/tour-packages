bb372f8b7847eac67d4151394fcce809fb420c35
siddhi22rachit <siddhibswant2212@gmail.com>
2024-12-16 23:21:29 +0530

    for adding

--- Backend/models/Package.js
+++ Backend/models/Package.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 
 const packageSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String },
+  description: { type: String, required: true },
   price: { type: Number, required: true },
   duration: { type: String, required: true },
   availableDates: { type: [String], required: true },
   image: { type: String, required: true },
-  highlights: { type: [String] },
+  highlights: { type: [String], required: true },
 });
 
 const Package = mongoose.model('Package', packageSchema);
